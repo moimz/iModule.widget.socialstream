6c06d66bb2e385c1c5d48fb1f0239f0490d7e8c9
arzzcom <arzz@arzz.com>
2018-12-14 13:36:32 +0900

    고정크기 템플릿 추가

---
+++ templets/fixed/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 이 파일은 소셜스트림 위젯의 일부입니다. (https://www.imodules.io)
+ * 
+ * 소셜스트림 위젯 고정크기 템플릿
+ *
+ * @file /widgets/socialstream/templets/fixed/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0
+ * @modified 2018. 12. 14.
+ */
+if (defined('__IM__') == false) exit;
+?>
+<div data-role="loading"><i class="mi mi-loading"></i></div>
